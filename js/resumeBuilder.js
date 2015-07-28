00aeb20ecc15351c3e305c9a1ba2091e2ea277ff
DoctaDocta <trashm96@gmail.com>
2015-07-28 17:22:04 -0400

    4 jsons for site info

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,8 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
+<script src="js/resumedata.js"></script>
+<script src="js/helper.js"></script>
+
+HTMLheaderName.replace("%data%", bio.name);
+$('#header').append(HTMLheaderName);
---
+++ js/resumedata.js
@@ -0,0 +1,56 @@
+/* 4 JSON objects that will fill my resume website*/
+
+var bio = {
+	"name": "Andre",
+	"role": "Engineer",
+	"contacts": {
+		"mobile": "732-307-8997",
+    	"email": "andreehrlich2018@u.northwestern.edu",
+    	"github": "DoctaDocta",
+    	"twitter": "none",
+    	"location": "Chicago"
+	},
+
+	"welcomeMessage": "Hello, thanks for visiting my website!",
+	"skills": ["matlab", "python", "html", "css", "javascript"],
+	"biopic": "http://dealbreaker.com/uploads/2015/04/Musky.jpg",
+	"display": "function"
+};
+
+var education = {
+	"schools": {
+		"name": "Northwestern University",
+		"location": "Evanston, IL",
+		"degree": "Bachelor of Science",
+		"majors": "Computer Engineering",
+		"dates": 2018,
+		"url": "http://www.northwestern.edu"
+	},
+	"onlineCourses": {
+		"title": "Front-end Web Developer Nanodegree",
+		"school": "Udacity",
+		"dates": 2018,
+		"url": "udacity.com"
+	},
+	"display": "function"
+};
+
+
+var work = {
+	"jobs": {
+			"employer": "Lora Sgroi",
+			"title": "Sailing Instructor",
+			"location": "Fair Haven, NJ",
+			"dates": "June-Aug, 2013 and 2014",
+			"description": "Worked with a team"
+	},
+	"display": "function"
+};
+
+var projects = {
+	"title": "iTunes Album Artwork ",
+	"dates": "July 2015",
+	"description": "Python script reads iTunes XML, performs a google \n
+		search query, and embeds images into mp3 files",
+	"display": "function"
+};
